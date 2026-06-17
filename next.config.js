995eddeda3340f962999bd1a2c16022bf830eb5d
OperacionesAdipa <operaciones@adipa.cl>
2026-06-17 16:19:25 -0400

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,7 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ['adipa.zendesk.com'],
+    domains: [
+      'adipa.zendesk.com',
+      'zen-marketing-documentation.s3.amazonaws.com',
+      'help.adipa.cl',
+      'lh3.googleusercontent.com',
+      'downloads.intercomcdn.com',
+    ],
   },
 }
 
