cf234ba511660c228fe177260887e4dce8933774
OperacionesAdipa <operaciones@adipa.cl>
2026-07-01 21:26:04 -0400

    Update page.tsx

--- src/app/articulo/[slug]/editar/page.tsx
+++ src/app/articulo/[slug]/editar/page.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { useEffect } from 'react'
+import { useEffect } from 'use'
 import { useRouter } from 'next/navigation'
 
 export default function EditarDesdeArticuloPage({ params }: { params: { slug: string } }) {
