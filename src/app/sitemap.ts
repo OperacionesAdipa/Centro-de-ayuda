03e47167837765a1455b23b420c88a059ca94db2
OperacionesAdipa <operaciones@adipa.cl>
2026-07-30 13:04:16 -0400

    Update sitemap.ts

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,6 +1,8 @@
 import { MetadataRoute } from 'next'
 import { getArticles, getCategories, slugify } from '@/lib/supabaseQueries'
 
+export const dynamic = 'force-dynamic'
+
 export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
   const baseUrl = 'https://centro-de-ayuda-eta.vercel.app'
 
